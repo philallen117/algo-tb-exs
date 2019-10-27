da69ab73e8cf42f07e9f22f92650cc8ac7418b52
Phil Allen <phil.allen.gm@gmail.com>
2019-10-27 11:42:52 +0000

    w1 and w2 for completeness

--- w4/binary_search.py
+++ w4/binary_search.py
@@ -1,9 +1,37 @@
 # Uses python3
 import sys
 
+def binary_search_r(a, x):
+    def aux(left, right):
+        if left > right:
+            return -1
+        else:
+            mid = (left + right) // 2
+            amid = a[mid]
+            if amid == x:
+                return mid
+            elif x > amid:
+                return aux(mid + 1, right)
+            else:
+                return aux(left, mid - 1)
+    return aux(0, len(a) - 1)
+        
 def binary_search(a, x):
-    left, right = 0, len(a)
-    # write your code here
+    left, right = 0, len(a) - 1
+    while True:
+        if left > right:
+            return -1
+        else:
+            mid = (left + right) // 2
+            amid = a[mid]
+            if amid == x:
+                return mid
+            elif x > amid:
+                left = mid + 1
+                continue
+            else:
+                right = mid - 1
+                continue
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -20,3 +48,4 @@ if __name__ == '__main__':
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
         print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
