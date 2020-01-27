f3895dd5f450da10ffa32ca6a98a79a69767079d
Phil Allen <phil.allen.gm@gmail.com>
2020-01-27 00:41:34 +0000

    w5 first attempt

--- w5/lcs2.py
+++ w5/lcs2.py
@@ -2,9 +2,38 @@
 
 import sys
 
-def lcs2(a, b):
-    #write your code here
-    return min(len(a), len(b))
+class TwoDArray:
+    # 0-indexed
+    def __init__(self, rows, cols):
+        super().__init__()
+        self.rows = rows
+        self.cols = cols
+        self.content = [0] * (rows * cols)
+
+    def get(self, i, j):
+        return self.content[i * self.cols + j]
+
+    def set(self, i, j, value):
+        self.content[i * self.cols + j] = value
+
+def lcs2(s, t):
+    m, n = len(s), len(t)
+    lcs = TwoDArray(m + 1, n + 1)
+    # distance table - with s on rows and t on columns
+    # for row in range(m + 1):
+    #     lcs.set(row, 0, 0)
+    # for col in range(n + 1):
+    #     lcs.set(0, col, 0)
+    for row in range(1, m + 1):
+        ss = s[row - 1]   # 0-indexed
+        for col in range(1, n + 1):
+            tt = t[col - 1]
+            u = lcs.get(row - 1, col - 1)
+            if ss == tt: u = u + 1
+            v = lcs.get(row - 1, col)
+            w = lcs.get(row, col - 1)
+            lcs.set(row, col, max(u, v, w))
+    return lcs.get(m - 1, n - 1)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- w5/lcs3.py
+++ w5/lcs3.py
@@ -2,8 +2,39 @@
 
 import sys
 
+class ThreeDArray:
+    # 0-indexed
+    def __init__(self, rows, cols, ranks):
+        super().__init__()
+        self.rows = rows
+        self.cols = cols
+        self.ranks = ranks
+        self.content = [0] * (rows * cols * ranks)
+
+    def get(self, i, j, k):
+        return self.content[i * self.cols * self.ranks + j * self.ranks + k]
+
+    def set(self, i, j, k, value):
+        self.content[i * self.cols * self.ranks + j * self.ranks + k] = value
+
 def lcs3(a, b, c):
-    #write your code here
+    rows, cols, ranks = len(a) + 1, len(b) + 1, len(c) + 1
+    lcs = ThreeDArray(rows, cols, ranks)
+    # First row, col, rank correspond empty prefixes of a, b, c respectively.
+    # They are already initialized to 0.
+    for row in range(1, rows):
+        aa = a[row - 1]   # 0-indexed
+        for col in range(1, cols):
+            bb = b[col - 1]
+            for rank in range(1, ranks):
+                cc = c[rank - 1]
+                u = lcs.get(row - 1, col - 1, rank - 1)
+                if aa == bb and bb == cc: u = u + 1 # match
+                v = lcs.get(row - 1, col, rank)
+                w = lcs.get(row, col - 1, rank)
+                x = lcs.get(row, col, rank - 1)
+                lcs.set(row, col, rank, max(u, v, w, x))
+    return lcs.get(rows - 1, cols - 1, ranks - 1)
     return min(len(a), len(b), len(c))
 
 if __name__ == '__main__':
